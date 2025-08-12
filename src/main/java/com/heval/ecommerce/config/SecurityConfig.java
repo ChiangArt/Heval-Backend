d3a0c24be329e3b6983028ff70b886c1099ca09e
Bryan Lee Chiang Arteaga <bryan_94tj@hotmail.com>
2025-08-12 14:17:54 -0500

    agregando cambios

--- src/main/java/com/heval/ecommerce/config/SecurityConfig.java
+++ src/main/java/com/heval/ecommerce/config/SecurityConfig.java
@@ -81,7 +81,6 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("https://hevalshop.com"));
-        configuration.addAllowedOriginPattern("*");
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("Authorization"));
