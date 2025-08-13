c08b396b8bb0a2d9fe3570bc4945418f17246c56
Bryan Lee Chiang Arteaga <bryan_94tj@hotmail.com>
2025-08-13 13:05:22 -0500

    agregando neuvos cambios

--- src/main/java/com/heval/ecommerce/services/BannerService.java
+++ src/main/java/com/heval/ecommerce/services/BannerService.java
@@ -1,6 +1,10 @@
 package com.heval.ecommerce.services;
 
 import com.heval.ecommerce.entity.Banner;
+import com.heval.ecommerce.repository.BannerRepository;
 
 public interface BannerService extends CrudService<Banner, Long> {
-}
+
+    void deleteBanner(Long id);
+
+    }
--- src/main/java/com/heval/ecommerce/services/impl/BannerServiceImpl.java
+++ src/main/java/com/heval/ecommerce/services/impl/BannerServiceImpl.java
@@ -1,31 +1,50 @@
 package com.heval.ecommerce.services.impl;
-
 import com.heval.ecommerce.entity.Banner;
 import com.heval.ecommerce.exception.ApiValidateException;
 import com.heval.ecommerce.repository.BannerRepository;
 import com.heval.ecommerce.services.BannerService;
+import com.heval.ecommerce.services.S3Service;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BannerServiceImpl extends CrudServiceImpl<Banner, Long> implements BannerService {
-
     private final BannerRepository bannerRepository;
+    private final S3Service s3Service;
 
-
-    public BannerServiceImpl(BannerRepository repository) {
+    public BannerServiceImpl(BannerRepository repository, S3Service s3Service) {
         super(repository);
         this.bannerRepository = repository;
-
+        this.s3Service = s3Service;
     }
 
     @Override
     @Transactional
     public Banner save(Banner entity) {
         long count = bannerRepository.count();
-        if (count >= 1) {
+        if (count >= 2) {
             throw new ApiValidateException("Solo se permiten 2 banners.");
         }
         return super.save(entity);
+    }
+
+    @Override
+    @Transactional
+    public void deleteBanner(Long id) {
+        Banner banner = bannerRepository.findById(id)
+                .orElseThrow(() -> new ApiValidateException("Banner no encontrado con id: " + id));
+
+        if (banner.getUrls() != null) {
+            for (String imageUrl : banner.getUrls()) {
+                try {
+                    String key = s3Service.extractKeyFromUrl(imageUrl);
+                    s3Service.deleteFile(key);
+                } catch (Exception e) {
+                    System.err.println("Error eliminando archivo S3: " + imageUrl + " -> " + e.getMessage());
+                }
+            }
+        }
+
+        bannerRepository.delete(banner);
     }
 }
